6de357425df55abf8c41e27d0cffe9eef8d72a79
luvumbu <luvumbu.n@gmail.com>
2019-07-11 14:47:01 +0200

    Correction des Bugs

--- src/app/app.js
+++ src/app/app.js
@@ -7,25 +7,8 @@ let body =
 let app_header  = document.getElementsByTagName(body.header);
 let app_section = document.getElementsByTagName(body.section);
 let app_footer  = document.getElementsByTagName(body.footer);
-
-
-
-function loadDoc() {
-        var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function() {
-          if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("demo").innerHTML = this.responseText;
-          }
-        };
-        app_header[0].style.display="none";
-        xhttp.open("GET", "ajax_info.txt", true);
-        xhttp.send();
-      }
-
-
 var CheminComplet = document.location.href;
 var NomDuFichier  = CheminComplet.substring(CheminComplet.lastIndexOf( "/" )+1 );
-
-
+console.log(NomDuFichier) ; // affiche le nom de l'url 
 
 
